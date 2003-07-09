3e4adaee9b07113d83a9984259aca561ed6c2657
torvalds <torvalds>
2003-07-09 05:55:17 +0000

    Merge bk://kernel.bkbits.net/davem/net-2.5 into home.osdl.org:/home/torvalds/v2.5/linux
    2003/07/08 19:37:53-07:00 jmorris
    [IPSEC]: Do not call request_module() under spinlock in xfrm_get_type().
    
    2003/07/08 19:10:11-07:00 davem
    Merge bk://kernel.bkbits.net/jmorris/ipv6-2.5
    into nuts.ninka.net:/home/davem/src/BK/net-2.5
    
    2003/07/08 18:24:04-07:00 davem
    Resolve conflicts in ipconfig.c changes.
    
    2003/07/09 10:30:18+10:00 rddunlap
    [IPV6] use correct mib struct
    
    And spell it correctly :-)
    
    2003/07/09 01:23:54+10:00 yoshfuji
    [IPV6] Fix BUG when appending destination options headers
    
    This patch fixes BUG when pushing IPv6 destination options over an
    IPv6 raw socket.  Patch is based on one from Thomas Graf <tgr@reeler.org>.
    
    2003/07/07 10:55:20+10:00 herbert
    [IPSEC] Add policy expiration
    
    This patch finally adds policy expiration.
    
    Note that it resends soft policy expire messages every 30 seconds.  This
    is needed as when "soft use expire" is used for dead peer detection,
    a lost message could lead to a dead peer that isn't discovered until the
    SAs expire.
    
    I've only implemented notification for XFRM as I didn't want to just add
    another PFKEY extension in case it collides with something else.  Of
    course it could be easily done for PFKEY with an extension too.
    
    2003/07/07 10:41:01+10:00 dtor_core
    [NET] Attach inner qdiscs to TBF
    
    2003/07/06 11:34:52+10:00 lode_leroy
    [IPV4] display bootserver in /proc/net/pnp
    
    With modification from niv@us.ibm.com to only print
    bootserver if there was one.
    
    2003/07/06 03:52:26+10:00 yoshfuji
    [ATM] Convert clip neigh table to C99 initializers
    
    This converts nlip_tbl to C99 initializers.
    (and fixes wrong value for proxy_len and locktime.)
    
    2003/07/06 03:36:27+10:00 yoshfuji
    [IPV6] fix algorithm for updating lifetime
    
    This patch, depends on "IPV6: remove unused variable,"
    fixes algorithm for updating lifetime according to RFC2462.
     - We should not delete an address even if prefix is
       advertised with valid lifetime == 0.
     - support the "2 hours" rule.
    
    2003/07/06 03:35:30+10:00 yoshfuji
    [IPV6] remove unused variable
    
    2003/07/06 03:34:40+10:00 yoshfuji
    [IPV6] ignore on-link information without on-link flag set
    
    On-link flag == 0 in RA message does not mean that the
    prefix is off-link. It simply means the prefix
    does not talk about on-link information.
    
    2003/07/06 03:32:45+10:00 yoshfuji
    [NET] Send only unicast NSs in PROBE state.
    
    2003/07/03 20:02:25+10:00 yoshfuji
    [NET] Fix oops with /proc/net/{raw,igmp,mfilter,
    raw6,igmp6,mfilter6,anycast,ip6_flowlabel}.
    
    2003/07/03 10:53:47+10:00 yoshfuji
    [IPV6] fix a mistake in ipv6_advmss() conversion
    
    2003/07/03 01:40:43+10:00 lunz
    [NET] Fix refcounting of dev->promiscuity for af_packet.
    
    BKrev: 3f0bae45pjj9eHE4J0_oBauR_5sR4Q

--- net/ipv4/ipconfig.c
+++ net/ipv4/ipconfig.c
@@ -1115,8 +1115,10 @@ static int pnp_get_info(char *buffer, char **start,
 				       "nameserver %u.%u.%u.%u\n",
 				       NIPQUAD(ic_nameservers[i]));
 	}
-	len += sprintf(buffer + len, "bootserver %u.%u.%u.%u\n",
-			NIPQUAD(ic_servaddr));
+	if (ic_servaddr != INADDR_NONE)
+		len += sprintf(buffer + len,
+			       "bootserver %u.%u.%u.%u\n",
+			       NIPQUAD(ic_servaddr));
 
 	if (offset > len)
 		offset = len;
